fc9988092fb8b81f97f2851d8508f2bcfb6b28cb
Lucas <silvestri.luca@outlook.com>
2023-11-17 08:42:12 +0100

    progressi

--- phase1/msg.c
+++ phase1/msg.c
@@ -6,6 +6,9 @@ static msg_t msgTable[MAXMESSAGES];
 LIST_HEAD(msgFree_h);
 
 void initMsgs() {
+    for(int i=0;i<MAXMESSAGES;i++){
+        list_add(&msgTable[i], &msgFree_h);
+    }
 }
 
 void freeMsg(msg_t *m) {
@@ -14,14 +17,14 @@ void freeMsg(msg_t *m) {
 }
 
 msg_t *allocMsg() {
-    if(list_empty(msgFree_h) == true)
+    if(list_empty(&msgFree_h) == TRUE)
         return NULL;
     else{
-        list_del(msgFree_h);
+        list_del(&msgFree_h);
         for(int i=0;i<MAXMESSAGES;i++){
             msgTable[i].m_payload = 0;
         }
-        return msgFree_h;
+        return &msgFree_h;
     }
 }
 
@@ -29,9 +32,14 @@ void mkEmptyMessageQ(struct list_head *head) {
 }
 
 int emptyMessageQ(struct list_head *head) {
+    if(list_empty(head) == TRUE)
+        return 1;
+    else
+        return 0;
 }
 
 void insertMessage(struct list_head *head, msg_t *m) {
+    list_add_tail(m, head);
 }
 
 void pushMessage(struct list_head *head, msg_t *m) {
