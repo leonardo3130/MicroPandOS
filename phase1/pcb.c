0c6ac3c9d22e871d349015896a6a2212fe6915da
leonardo3130 <leonardo313@gmail.com>
2023-11-29 13:51:10 +0100

    .

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -108,8 +108,6 @@ pcb_t *outProcQ(struct list_head *head, pcb_t *p) {
     }
     return NULL;
 }
-//p_child del padre = sentinella lista p_sib (figli) --> quando iterando la lista incontro l'elemento che è padre degli altri
-//sono arrivato alla testa --> sentinella --> posso effettuare le operazioni (definite in listx.h) che richiedono la testa
 
 int emptyChild(pcb_t *p) {
     return list_empty(&(p -> p_child)); //controllo che la lista dei figli sia vuota
