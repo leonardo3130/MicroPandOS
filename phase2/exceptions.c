6e0acd82c20acc86f15ce177560102f6a2361d9b
leonardo3130 <leonardopo313@gmail.com>
2024-02-17 12:09:54 +0100

    add syscall exception handler

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -23,7 +23,53 @@ static void syscallExceptionHandler(state_t* exceptionState){
     passUpOrDie(GENERALEXCEPT, exceptionState);
   }
   else {
-    //qualcosa con SSI ?
+    //devo capire se la roba qu sotto viene fatta dalla routine SYSCALL o se devo farla io
+    if(exceptionState->reg_a0 == SENDMESSAGE) {
+      //SEND is async
+      pcb_t *dest = (pcb_t *)(exceptionState->reg_a1);
+      
+      if(dest = outProcQ(&Locked_Message, dest)) { //Locked_Message not defined yet --> processes waiting for a Locked_Message
+        insertProcQ(&Ready_Queue, dest);
+      } 
+      else{
+        int found = 0;
+        pcb_t *tmp;
+        list_for_each_entry(tmp, Ready_Queue, p_list) {
+          if(tmp == dest) {
+            found = 1;
+            break;
+          }
+        }
+        if(!found)
+          dest = NULL;
+        else {
+          msg_t *msg = allocMsg();
+          msg->payload = exceptionState->reg_a2;
+          msg->sender = Current_Process; //non sono sicuro
+          insertMessage(&dest->msg_list, msg);
+        }
+      }
+      if(!dest) 
+        exceptionState->reg_v0 = DEST_NOT_EXISTS;
+      else 
+        exceptionState->reg_v0 = 0;
+      exceptionState->pc_epc += WORDLEN;
+      LDST(exceptionState);
+    }
+    else if(exceptionState->reg_a0 == RECEIVEMESSAGE) {
+      list_head *msg_inbox = &(((pcb_t *)(exceptionState->reg_v0))->msg_list);
+      if(list_empty(msg_inbox) || !popMessage(msg_inbox, (pcb_t *)(exceptionState->reg_v0))) { //se non ci sono messaggi in attesa di essere ricevuti --> bloccante
+        insertProcQ(&Locked_Message, Current_Process); //blocco del processo, non sono sicuro di Current_Process
+        Current_Process->p_s = exceptionState;
+        //update cpu time
+        scheduler();
+      }
+      else {
+        exceptionState->pc_epc += WORDLEN;
+        LDST(exceptionState);
+      }
+    }
+    
   }
 }
 
@@ -40,8 +86,6 @@ void exceptionHandler() {
       passUpOrDie(PGFAULTEXCEPT, exceptionState);
 			break;
 		case SYSEXCEPTION:
-			//anche qui potrebbe sevire passUpOrDie ma la situa vs gestita
-      //diversamente
       syscallExceptionHandler(exceptionState);
 			break;
 		default: //4-7, 9-12
@@ -50,8 +94,6 @@ void exceptionHandler() {
 			break;
  }
 }
-
-
 
 void uTLB_RefillHandler() {
   setENTRYHI(0x80000000);
