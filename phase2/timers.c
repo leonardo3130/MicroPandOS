7b1fcffb0d49fc464c8a7679e2522db6bd59a865
leonardo3130 <leonardopo313@gmail.com>
2024-03-31 18:17:07 +0200

    aggiunti commenti in timers.c

--- phase2/timers.c
+++ phase2/timers.c
@@ -1,27 +1,18 @@
 #include "include/timers.h"
 
-//TOD --> time of day clock, readonly, per l'accesso serve kernel mode
-//accesso al suo valore --> STCK(int), non genera interrupt
-//
-//Interval Timer --> 32-bit unsigned int decrementato ogni ciclo, settato al boot
-//a 0xFFFFFFFF, genera interrupt su linea 2 con la transizione 0x00000000 --> 0xFFFFFFFF
-//lettura/scrittura richiedono kernel mode, eseguite tramite:
-//- LDIT(t) --> t diventa nuovo valore dle timer --> solo scrittura
-//- accesso diretto a Bus Register Memory Location = 0x10000020 (per lettura e scrittura)
-//
-//PLT --> processor local timer --> ogni processo ha il suo, impkementato con CP0 timer Register
-//decrementato ad ogni clock, interrupt con transizione: 0x00000000 --> 0xFFFFFFFF
-//interrupt che può essere disabilitato, a differenza di interval timer
-//accesso in lettura/scrittura con getTIMER e setTIMER
+/*
+ * semplice libreria che funge da wrapper 
+ * per le funzioni di utilizzo dei vari timer
+ */
 
-//questa libreria è un semplice wrapper per le funzioni sui timer
-
+//ritorna il valore del TOD al momento della chiamata
 unsigned int getTOD() {
   unsigned int tmp;
   STCK(tmp);
   return tmp;
 }
 
+//aggiorna p_time del processo p
 void updateCPUtime(pcb_t *p) {
   int end = getTOD();
   p->p_time += (end - start);
